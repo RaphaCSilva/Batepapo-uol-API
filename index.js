230abbf6879225a19a46c17229e21ceddc333423
Raphacsilva <raphaelcavalcantesilva1999@gmail.com>
2022-06-27 02:47:52 -0300

    conectando no banco

--- index.js
+++ index.js
@@ -1,14 +1,23 @@
-import express from 'express';
+import express, {json} from 'express';
 import dotenv from 'dotenv';
 import cors from 'cors';
 import { MongoClient } from 'mongodb';
 import chalk from 'chalk';
 
-const app = express();
 dotenv.config();
 
+const app = express();
+app.use(cors());
+app.use(json());
 
-
+let db;
+const mongoClient = new MongoClient("mongodb://localhost:27017");
+const promise = mongoClient.connect();
+promise.then(() => {
+  db = mongoClient.db("bate-papo-uol");
+  console.log("conectou no banco");
+});
+promise.catch((e) => console.log("erro na conexão com o banco", e))
 
 const porta = 5000;
 app.listen(porta, ()=> {
