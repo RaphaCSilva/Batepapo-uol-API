022ba319bfaeb6b851e2f01ae1446d503f31cd70
Raphacsilva <raphaelcavalcantesilva1999@gmail.com>
2022-06-27 03:30:15 -0300

    post/participants

--- index.js
+++ index.js
@@ -3,6 +3,8 @@ import dotenv from 'dotenv';
 import cors from 'cors';
 import { MongoClient } from 'mongodb';
 import chalk from 'chalk';
+import dayjs from "dayjs";
+import joi from "joi";
 
 dotenv.config();
 
@@ -18,6 +20,44 @@ promise.then(() => {
   console.log("conectou no banco");
 });
 promise.catch((e) => console.log("erro na conexão com o banco", e))
+
+app.post("/participants", async (req, res) => {
+    const participante = req.body; //{name: "joão"}
+    const participanteSchema = joi.object({
+        name: joi.string().required()
+    });
+    const {error} = participanteSchema.validate(participante, { abortEarly: false });
+    if(error){
+        console.log(error);
+        return res.sendStatus(422);
+    }
+    
+    try {
+        const userJaExiste = await db.collection("participantes").findOne({name: participante.name});
+        if(userJaExiste){
+            return res.sendStatus(409);
+        }
+        await db.collection("participantes").insertOne({name: participante.name, lastStatus: Date.now()})
+        await db.collection("mensagens").insertOne({
+            from: participante.name,
+            to: 'Todos',
+            text: 'entra na sala ...',
+            type: 'status',
+            time: dayjs().format('HH:MM:SS')
+        })
+
+        res.sendStatus(201);
+
+
+    } catch (e) {
+        console.log(e);
+        return res.send("Erro ao registrar");
+    }
+
+});
+
+
+
 
 const porta = 5000;
 app.listen(porta, ()=> {
